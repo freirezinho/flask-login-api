50f010b1f60d88fd2a33bb3b85f60184c22735fd
Saulo Freire <gbsaulo@gmail.com>
2021-05-22 16:00:35 -0300

    CORS + port change

--- app/controller/default.py
+++ app/controller/default.py
@@ -1,5 +1,5 @@
 from urllib import response
-from flask import Flask, request, url_for, redirect, Response, json
+from flask import Flask, request, url_for, redirect, Response, json, make_response
 from werkzeug.datastructures import Headers
 from flask_sqlalchemy import SQLAlchemy
 from app import app, db, bcrypt
@@ -43,12 +43,22 @@ def register():
         print('Erro inesperado: ', sys.exc_info[0])
         return Response(status=500)
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST', 'OPTIONS'])
 def login():
+    if request.method == "OPTIONS": #CORS treatment
+        return _build_cors_prelight_response()
     headers = Headers()
     headers.add('Content-Type', 'application/json')
     login_obj = request.json
-
+    print('will validate request')
+    if login_obj == None:
+        print("Empty request")
+        print(request.json)
+        return Response(
+            response=json.dumps({"msg": "Invalid request"}),
+            status=400,
+            headers=headers
+        )
     isRequestValid = validateUserData(login_obj, 'login')
     username_req = login_obj['username'] if "username" in login_obj else login_obj['email']
     password_req = login_obj['password']
@@ -100,7 +110,9 @@ def validateUserData(userData: dict, endpoint: str):
     elif endpoint == 'login':
         try:
             if "email" in userData and "password" in userData:
+                print("Checking object...")
                 password_req = userData['password']
+                print("Checking object keys filling...")
                 if password_req == None or userData['email'] == None: return False
                 return True
             elif "username" in userData and "password" in userData:
@@ -110,3 +122,18 @@ def validateUserData(userData: dict, endpoint: str):
                 return True
         except KeyError:
             return False
+
+@app.after_request
+def after_request(response):
+    header = response.headers
+    header['Access-Control-Allow-Origin'] = '*'
+    header['Access-Control-Allow-Headers'] = "*"
+    return response
+
+def _build_cors_prelight_response():
+    print('Returning CORS response')
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
